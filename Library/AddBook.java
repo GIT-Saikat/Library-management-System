59b494c1432fb25e7de30136b71f1f5035c75991
GIT-Saikat <saikatmondal2105@gmail.com>
2025-08-20 23:18:19 +0530

    Final 1

--- Library/AddBook.java
+++ Library/AddBook.java
@@ -15,24 +15,25 @@ public class AddBook implements IOOperation{
             return;
         }else{
             book.setName(sc.next());
-        }
-    
-        System.out.println("Enter book author: ");
-        book.setAuthor(sc.next());
-        System.out.println("Enter book publisher: ");
-        book.setPublisher(sc.next());
-        System.out.println("Enter book collection address: ");
-        book.setAddress(sc.next());
-        System.out.println("Enter qty: ");
-        book.setQty(sc.nextInt());
-        System.out.println("Enter price: ");
-        book.setPrice(sc.nextDouble());
-        System.out.println("Enter borrowing copies: ");
-        book.setBrwCopies(sc.nextInt());
-        database.AddBook(book);
-        System.out.println("Book added Successfully!!!\n");
-
-        user.menu(database, user);
-        sc.close();
+            
+            System.out.println("Enter book author: ");
+            book.setAuthor(sc.next());
+            System.out.println("Enter book publisher: ");
+            book.setPublisher(sc.next());
+            System.out.println("Enter book collection address: ");
+            book.setAddress(sc.next());
+            System.out.println("Enter qty: ");
+            book.setQty(sc.nextInt());
+            System.out.println("Enter price: ");
+            book.setPrice(sc.nextDouble());
+            System.out.println("Enter borrowing copies: ");
+            book.setBrwCopies(sc.nextInt());
+            database.AddBook(book);
+            System.out.println("Book added Successfully!!!\n");
+            }
+        
+        
+            user.menu(database, user);
+            sc.close();
     }
 }
